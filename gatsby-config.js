2872aef3f87cb7bc6b856e57ee93364f4948360c
Yvonne Nkr <yvonnkr86@gmail.com>
2020-01-05 20:10:13 +0000

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    title: `personal site`,
+    description: `personal site built with gatsby using headless cms Strapi`,
+    author: `@yvonnkr`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/logo.jpg`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
